0b85683f53c240593a2d1d871ff22431ce5e6aa0
Chris Weyl <cweyl@ibm.com>
2023-09-13 17:12:10 -0500

    Appease shfmt

--- scripts/format.bash
+++ scripts/format.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 shfmt --language-dialect bash -i 4 --write \
-	./**/*
+    ./**/*
--- scripts/lint.bash
+++ scripts/lint.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 shellcheck --shell=bash --external-sources \
-	bin/* --source-path=lib/ \
-	lib/* \
-	scripts/*
+    bin/* --source-path=lib/ \
+    lib/* \
+    scripts/*
 
 shfmt --language-dialect bash --diff -i 4 \
-	./**/*
+    ./**/*
