b67a51a5dafeac272bfa797925d269f4f9645528
Chris Weyl <cweyl@ibm.com>
2023-09-13 16:21:13 -0500

    Initial implementation

--- lib/utils.bash
+++ lib/utils.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# heavily influenced by, and credit to,
+# https://github.com/ORCID/asdf-regctl/blob/main/lib/utils.bash
+
 set -euo pipefail
 
 # TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for regsync.
@@ -8,67 +11,76 @@ TOOL_NAME="regsync"
 TOOL_TEST="regsync version"
 
 fail() {
-	echo -e "asdf-$TOOL_NAME: $*"
-	exit 1
+    echo -e "asdf-$TOOL_NAME: $*"
+    exit 1
 }
 
 curl_opts=(-fsSL)
 
 # NOTE: You might want to remove this if regsync is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+    curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
 sort_versions() {
-	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+    sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+        LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
 list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    git ls-remote --tags --refs "$GH_REPO" |
+        grep -o 'refs/tags/.*' | cut -d/ -f3- |
+        sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if regsync has other means of determining installable versions.
-	list_github_tags
+    list_github_tags
 }
 
 download_release() {
-	local version filename url
-	version="$1"
-	filename="$2"
+    local version filename url
+    version="$1"
+    filename="$2"
 
-	# TODO: Adapt the release URL convention for regsync
-	url="$GH_REPO/archive/v${version}.tar.gz"
+    local platform
+    local arch_test
 
-	echo "* Downloading $TOOL_NAME release $version..."
-	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+    platform=$(uname | tr '[:upper:]' '[:lower:]')
+    arch_test=$(uname -m)
+
+    if [[ "$arch_test" = x86_64 ]]; then
+        local arch=amd64
+    else
+        local arch=$arch_test
+    fi
+
+    url="$GH_REPO/releases/download/v${version}/${TOOL_NAME}-${platform}-${arch}"
+
+    echo "* Downloading $TOOL_NAME release $version..."
+    curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
 install_version() {
-	local install_type="$1"
-	local version="$2"
-	local install_path="${3%/bin}/bin"
+    local install_type="$1"
+    local version="$2"
+    local install_path="${3%/bin}/bin"
 
-	if [ "$install_type" != "version" ]; then
-		fail "asdf-$TOOL_NAME supports release installs only"
-	fi
+    if [ "$install_type" != "version" ]; then
+        fail "asdf-$TOOL_NAME supports release installs only"
+    fi
 
-	(
-		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+    (
+        mkdir -p "$install_path"
+        cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert regsync executable exists.
-		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+        # TODO: Assert regsync executable exists.
+        local tool_cmd
+        tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+        test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
-		echo "$TOOL_NAME $version installation was successful!"
-	) || (
-		rm -rf "$install_path"
-		fail "An error occurred while installing $TOOL_NAME $version."
-	)
+        echo "$TOOL_NAME $version installation was successful!"
+    ) || (
+        rm -rf "$install_path"
+        fail "An error occurred while installing $TOOL_NAME $version."
+    )
 }
